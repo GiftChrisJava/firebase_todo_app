097a16f48f63cd3f56a12e7ff1d805466853245d
GiftChrisJava <chrisjava77@gmail.com>
2024-08-01 13:32:00 +0200

    created a log in page

---
+++ socialminiapp/lib/pages/login_page.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
