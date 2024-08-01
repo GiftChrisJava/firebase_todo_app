add612e19e71cbe9d2502652c1da8fd6da40f9d4
GiftChrisJava <chrisjava77@gmail.com>
2024-08-01 16:02:39 +0200

    creating login page paja

--- socialminiapp/lib/pages/login_page.dart
+++ socialminiapp/lib/pages/login_page.dart
@@ -1,45 +1,92 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:socialminiapp/components/button.dart';
 import 'package:socialminiapp/components/textfield.dart';
 
 class LoginPage extends StatelessWidget {
   // text controllers
-  TextEditingController emailController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
 
-  TextEditingController password = TextEditingController();
+  final TextEditingController password = TextEditingController();
 
   LoginPage({super.key});
 
+  // login
+  void login() {}
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Theme.of(context).colorScheme.background,
         body: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            // logo
-            Icon(
-              Icons.person,
-              size: 80,
-              color: Theme.of(context).colorScheme.inversePrimary,
-            ),
+          child: Padding(
+            padding: const EdgeInsets.all(25),
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              // logo
+              Icon(
+                Icons.person,
+                size: 80,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
 
-            const SizedBox(height: 25),
+              const SizedBox(height: 25),
 
-            // app name
-            Text(
-              'M  I N I M A L',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
+              // app name
+              Text(
+                'M  I N I M A L',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
 
-            // txt field
+              const SizedBox(height: 55),
 
-            MyTextField(
-                hintText: "Email",
-                obscureText: false,
-                controller: emailController),
-          ]),
+              // txt field
+              MyTextField(
+                  hintText: "Email",
+                  obscureText: false,
+                  controller: emailController),
+
+              const SizedBox(height: 18),
+
+              // txt field
+              MyTextField(
+                  hintText: "Password",
+                  obscureText: true,
+                  controller: emailController),
+
+              const SizedBox(height: 18),
+
+              // forgot password
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text("Forgot password",
+                      style: TextStyle(
+                          color: Theme.of(context).colorScheme.secondary)),
+                ],
+              ),
+              const SizedBox(height: 25),
+
+              // login button
+              MyButton(
+                text: "Login",
+                onTap: login,
+              ),
+              const SizedBox(height: 25),
+
+              // do not have an account ?
+              Row(
+                children: [
+                  Text("Don't have an account?"),
+                  Text(
+                    "Register Here",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              )
+            ]),
+          ),
         ));
   }
 }
